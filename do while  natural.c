e9341be3d7eebfb8e307224212203f613b960d87
dhrumil-git <ghelanidhrumil@gmail.com>
2025-08-25 10:41:38 +0530

    Add files via upload

---
+++ do while  natural.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+printf("ënter a value.");
+int n;
+scanf("%d",&n);
+int i=0;
+do
+{
+    ++i;
+    printf("%d\n",i);
+
+}while(i < n);
+}
+
+
+
+
+
+
+
